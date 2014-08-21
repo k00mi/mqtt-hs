63261be1c748a7adb7653211e05b69c10afa570e
Lukas Braun <koomi@hackerspace-bamberg.de>
2014-08-22 00:47:47 +0200

    Adjust MQTT.Parser to new types.

--- MQTT/Parser.hs
+++ MQTT/Parser.hs
@@ -1,3 +1,4 @@
+{-# Language OverloadedStrings #-}
 {-|
 Module: MQTT.Parsers
 Copyright: Lukas Braun 2014
@@ -20,7 +21,7 @@ import Data.Text.Encoding (decodeUtf8')
 import Data.Word
 import Prelude hiding (takeWhile, take)
 
-import MQTT.Types
+import MQTT.Types hiding (body)
 
 -- | Type of a parser that also keeps track of the remaining length.
 type MessageParser a = StateT Word32 Parser a
@@ -28,33 +29,30 @@ type MessageParser a = StateT Word32 Parser a
 -- | Parse any MQTT message.
 message :: Parser Message
 message = do
-    header <- mqttHeader
+    (msgType, header) <- mqttHeader
     remaining <- parseRemaining
-    body header remaining
+    body header msgType remaining
 
-body :: MqttHeader -> Word32 -> Parser Message
-body header remaining =
-    let combine varHeaderP payloadP = Message header <$> varHeaderP <*> payloadP
-        noop = pure Nothing
-        parser =
-          case msgType header of
-            CONNECT      -> do varHeader <- connectHeader
-                               Message header (Just varHeader)
-                                 <$> fmap Just (connectPL varHeader)
-            CONNACK      -> combine connackHeader noop
-            PUBLISH      -> combine (publishHeader header) (fmap Just publishPL)
-            PUBACK       -> combine msgIDHeader noop
-            PUBREC       -> combine msgIDHeader noop
-            PUBREL       -> combine msgIDHeader noop
-            PUBCOMP      -> combine msgIDHeader noop
-            SUBSCRIBE    -> combine msgIDHeader (fmap Just subscribePL)
-            SUBACK       -> combine msgIDHeader (fmap Just subackPL)
-            UNSUBSCRIBE  -> combine msgIDHeader (fmap Just unsubscribePL)
-            UNSUBACK     -> combine msgIDHeader noop
-            PINGREQ      -> combine noop noop
-            PINGRESP     -> combine noop noop
-            DISCONNECT   -> combine noop noop
-    in evalStateT parser remaining
+body :: MqttHeader -> Word8 -> Word32 -> Parser Message
+body header msgType remaining =
+    let parser =
+          case msgType of
+            1  -> MConnect      <$> connect
+            2  -> MConnAck      <$> connAck
+            3  -> MPublish      <$> publish header
+            4  -> MPubAck       <$> simpleMsg
+            5  -> MPubRec       <$> simpleMsg
+            6  -> MPubRel       <$> simpleMsg
+            7  -> MPubComp      <$> simpleMsg
+            8  -> MSubscribe    <$> subscribe
+            9  -> MSubAck       <$> subAck
+            10 -> MUnsubscribe  <$> unsubscribe
+            11 -> MUnsubAck     <$> simpleMsg
+            12 -> MPingReq      <$> simpleMsg
+            13 -> MPingResp     <$> simpleMsg
+            14 -> pure MDisconnect
+            t  -> lift $ fail ("Invalid message type: " ++ show t)
+    in Message header <$> evalStateT parser remaining
 
 
 ---------------------------------
@@ -62,14 +60,14 @@ body header remaining =
 ---------------------------------
 
 -- | Parser for the fixed header part of a MQTT message.
-mqttHeader :: Parser MqttHeader
+mqttHeader :: Parser (Word8, MqttHeader)
 mqttHeader = do
     byte1 <- anyWord8
-    msgType <- toMsgType $ shiftR byte1 4
     qos <- toQoS $ 3 .&. shiftR byte1 1
     let retain = testBit byte1 0
         dup = testBit byte1 3
-    return $ Header msgType dup qos retain
+        msgType = shiftR byte1 4
+    return (msgType, Header dup qos retain)
 
 -- | Parse the 'remaining length' field that indicates how long the rest of
 -- the message is.
@@ -89,95 +87,95 @@ parseRemaining = do
 -- * Variable Headers
 ---------------------------------
 
-connectHeader :: MessageParser VarHeader
-connectHeader = do
-    protocol <- mqttText
-    version <- anyWord8'
+connect :: MessageParser Connect
+connect = do
+    protocol
+    version
+
     flags <- anyWord8'
     let clean = testBit flags 1
         willFlag = testBit flags 2
-        willRetain = if willFlag
-                      then Just (testBit flags 5)
-                      else Nothing
         usernameFlag = testBit flags 7
         passwordFlag = testBit flags 6
-    willQoS <- if willFlag
-                 then Just <$> toQoS (3 .&. shiftR flags 3)
-                 else return Nothing
+
     keepAlive <- anyWord16BE
-    return $ VHConnect $
-      ConnectHeader
-        protocol
-        version
-        clean
-        ((,) <$> willQoS <*> willRetain)
-        usernameFlag
-        passwordFlag
-        keepAlive
 
-connackHeader :: MessageParser (Maybe VarHeader)
-connackHeader = Just . VHConnAck <$> anyWord8'
+    clientID <- getClientID
 
-publishHeader :: MqttHeader -> MessageParser (Maybe VarHeader)
-publishHeader header =
-    Just . VHPublish <$> (PublishHeader
-                          <$> fmap toTopic mqttText
-                          <*> if qos header > NoConfirm
-                                then Just <$> msgID
-                                else pure Nothing)
+    mWill <- parseIf willFlag $
+               Will (testBit flags 5)
+                  <$> toQoS (3 .&. shiftR flags 3)
+                  <*> fmap toTopic mqttText
+                  <*> mqttText
 
--- | Parse a variable header that consists solely of a message ID
-msgIDHeader :: MessageParser (Maybe VarHeader)
-msgIDHeader = Just . VHOther <$> msgID
+    username <- parseIf usernameFlag mqttText
 
----------------------------------
--- * Payload
----------------------------------
+    password <- parseIf passwordFlag mqttText
 
--- | Parses the fields of a connect message payload as indicated in the
--- variable header.
-connectPL :: VarHeader -> MessageParser Payload
-connectPL (VHConnect connHeader) = do
-  before <- get
-  clientID <- mqttText
-  after <- get
-  when (before - after + 2 > 23) $
-    fail $ "Client ID must not be longer than 23 chars: " ++ show clientID
-  PLConnect <$>
-    (ConnectPL clientID
-      <$> (fmap toTopic <$> textIfSet (isJust . will))
-      <*> textIfSet (isJust . will)
-      <*> textIfSet usernameFlag
-      <*> textIfSet passwordFlag)
+    return $ Connect clean mWill clientID username password keepAlive
   where
-    textIfSet field = if field connHeader
-                        then Just <$> mqttText
-                        else pure Nothing
-connectPL varHeader =
-    fail $ "Trying to parse CONNECT payload after non-CONNECT variable header: "
-            ++ show varHeader
+    protocol = do
+      prot <- mqttText
+      when (prot /= "MQIsdp") $
+        fail $ "Invalid protocol: " ++ show prot
 
-publishPL :: MessageParser Payload
-publishPL = get >>= fmap PLPublish . take'
+    version = do
+      version <- anyWord8'
+      when (version /= 3) $
+        fail $ "Invalid version: " ++ show version
 
-subscribePL :: MessageParser Payload
-subscribePL = fmap PLSubscribe $
-    whileM ((0 <) <$> get) $
-      (,) <$> fmap toTopic mqttText <*> (anyWord8' >>= toQoS)
+    getClientID = do
+      before <- get
+      clientID <- mqttText
+      after <- get
+      let len = before - after - 2 -- 2 for length prefix
+      when (len > 23) $
+        fail $ "Client ID must not be longer than 23 chars: "
+                  ++ show (text clientID) ++ " (" ++ show len ++ ")"
+      return clientID
 
-subackPL :: MessageParser Payload
-subackPL = fmap PLSubAck $
-    whileM ((0 <) <$> get) $
-      anyWord8' >>= toQoS
+    parseIf :: Applicative f => Bool -> f a -> f (Maybe a)
+    parseIf flag parser = if flag then Just <$> parser else pure Nothing
 
-unsubscribePL :: MessageParser Payload
-unsubscribePL = PLUnsubscribe <$>
-    whileM ((0 <) <$> get) (fmap toTopic mqttText)
+
+connAck :: MessageParser ConnAck
+connAck = ConnAck <$> anyWord8'
+
+publish :: MqttHeader -> MessageParser Publish
+publish header = Publish
+                  <$> getTopic
+                  <*> (if qos header > NoConfirm
+                         then Just <$> getMsgID
+                         else return Nothing)
+                  <*> (get >>= take')
+
+subscribe :: MessageParser Subscribe
+subscribe = Subscribe
+              <$> getMsgID
+              <*> whileM ((0 <) <$> get)
+                    ((,) <$> getTopic <*> (anyWord8' >>= toQoS))
+
+subAck :: MessageParser SubAck
+subAck = SubAck
+          <$> getMsgID
+          <*> whileM ((0 <) <$> get) (anyWord8' >>= toQoS)
+
+unsubscribe :: MessageParser Unsubscribe
+unsubscribe = Unsubscribe
+                <$> getMsgID
+                <*> whileM ((0 <) <$> get) getTopic
+
+simpleMsg :: MessageParser SimpleMsg
+simpleMsg = SimpleMsg <$> getMsgID
 
 
 ---------------------------------
 -- * Utility functions
 ---------------------------------
+
+-- | Parse a topic name.
+getTopic :: MessageParser Topic
+getTopic = toTopic <$> mqttText
 
 -- | Parse a length-prefixed UTF-8 string.
 mqttText :: MessageParser MqttText
@@ -189,8 +187,8 @@ mqttText = do
       Right txt -> return $ MqttText txt
 
 -- | Synonym for 'anyWord16BE'.
-msgID :: MessageParser Word16
-msgID = anyWord16BE
+getMsgID :: MessageParser Word16
+getMsgID = anyWord16BE
 
 -- | Parse a big-endian 16bit integer.
 anyWord16BE :: (Num a, Bits a) => MessageParser a
@@ -225,22 +223,3 @@ toQoS 0 = return NoConfirm
 toQoS 1 = return Confirm
 toQoS 2 = return Handshake
 toQoS x = fail $ "Invalid QoS value: " ++ show x
-
--- | Convert a number to a 'MsgType'. Calls 'fail' if the number can't be
--- converted.
-toMsgType :: (Num a, Eq a, Show a, Monad m) => a -> m MsgType
-toMsgType 1  = return CONNECT
-toMsgType 2  = return CONNACK
-toMsgType 3  = return PUBLISH
-toMsgType 4  = return PUBACK
-toMsgType 5  = return PUBREC
-toMsgType 6  = return PUBREL
-toMsgType 7  = return PUBCOMP
-toMsgType 8  = return SUBSCRIBE
-toMsgType 9  = return SUBACK
-toMsgType 10 = return UNSUBSCRIBE
-toMsgType 11 = return UNSUBACK
-toMsgType 12 = return PINGREQ
-toMsgType 13 = return PINGRESP
-toMsgType 14 = return DISCONNECT
-toMsgType x  = fail $ "Invalid message type: " ++ show x
